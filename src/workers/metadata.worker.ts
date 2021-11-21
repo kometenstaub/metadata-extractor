12b60642b17707828a87833e1cc9fb84e9490d98
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-11-21 20:10:15 +0100

    refactor: :recycle: backlinks calculation less complex

--- src/workers/metadata.worker.ts
+++ src/workers/metadata.worker.ts
@@ -1,55 +1,35 @@
 import type { Metadata, backlinks } from 'src/interfaces';
 
-self.onmessage = function(e) {
-	let metadataCache: Metadata[] = e.data[0]
-	let backlinkObj: backlinks[] = e.data[1]
-	const newMetadataCache: Metadata[] = metadataCache
-	
+self.onmessage = function (e) {
+	let metadataCache: Metadata[] = e.data[0];
+	let backlinkObj: backlinks[] = e.data[1];
+	const newMetadataCache: Metadata[] = metadataCache;
+
 	metadataCache.forEach((file: Metadata) => {
 		const fileName = file.fileName;
 		const relativeFilePath = file.relativePath;
 		newMetadataCache.forEach((otherFile: Metadata) => {
+			//@ts-expect-error
+			let currentBacklinkObject: backlinks = {};
 			// don't check the same file
 			if (fileName !== otherFile.fileName) {
 				if (otherFile.links) {
 					otherFile.links.forEach((links) => {
+						// check if already present, only  push if not present
 						if (links.relativePath === relativeFilePath) {
-							// check if already present, only  push if not present
-							if (links.cleanLink && links.displayText) {
-								backlinkObj.push({
-									fileName: otherFile.fileName,
-									link: links.link,
-									relativePath: otherFile.relativePath,
-									cleanLink: links.cleanLink,
-									displayText: links.displayText,
-								});
-							} else if (
-								links.cleanLink &&
-								!links.displayText
-							) {
-								backlinkObj.push({
-									fileName: otherFile.fileName,
-									link: links.link,
-									relativePath: otherFile.relativePath,
-									cleanLink: links.cleanLink,
-								});
-							} else if (
-								!links.cleanLink &&
-								links.displayText
-							) {
-								backlinkObj.push({
-									fileName: otherFile.fileName,
-									link: links.link,
-									relativePath: otherFile.relativePath,
-									displayText: links.displayText,
-								});
-							} else {
-								backlinkObj.push({
-									fileName: otherFile.fileName,
-									link: links.link,
-									relativePath: otherFile.relativePath,
-								});
+							currentBacklinkObject.fileName = otherFile.fileName;
+							currentBacklinkObject.link = links.link;
+							currentBacklinkObject.relativePath =
+								otherFile.relativePath;
+							if (links.cleanLink) {
+								currentBacklinkObject.cleanLink =
+									links.cleanLink;
 							}
+							if (links.displayText) {
+								currentBacklinkObject.displayText =
+									links.displayText;
+							}
+							backlinkObj.push(currentBacklinkObject);
 						}
 					});
 				}
@@ -57,12 +37,9 @@ self.onmessage = function(e) {
 		});
 		if (backlinkObj.length > 0) {
 			file.backlinks = backlinkObj;
-		}// empty it, otherwise it would collect all of the links in the forEach loop
+		} // empty it, otherwise it would collect all of the links in the forEach loop
 		backlinkObj = [];
 	});
 
-	self.postMessage(metadataCache)
-}
-
-
-
+	self.postMessage(metadataCache);
+};
