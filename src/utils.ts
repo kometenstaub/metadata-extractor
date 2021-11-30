122fbf19ef73c43dcb979fdd5cf82d0b6616e06a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-11-30 14:05:14 +0000

    refactor: Move getAllExceptMd() to utils
    Co-Authored-By: kometenstaub <83140328+kometenstaub@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,17 @@
 import type { exceptMd, file, folder } from "./interfaces";
 
+export function getAllExceptMd(allFolders: folder[], allFiles: file[]) {
+	const otherFiles: file[] = [];
+	for (const TAFile of allFiles) {
+		// The basename is the name without the extension
+		if (TAFile.name.slice(-3) !== '.md') {
+			otherFiles.push(TAFile);
+		}
+	}
+	const foldersAndFiles = makeFolderAndFileObject(allFolders, otherFiles);
+	return foldersAndFiles;
+}
+
 export function makeFolderAndFileObject(folders: folder[], otherFiles: file[]): exceptMd {
 		//@ts-expect-error, it requires to be initialized, but values will only be added later,
 		// but they are required by TS
